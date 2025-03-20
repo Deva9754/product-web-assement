358adc3935f274bb873759c416a1300c76c0d99a
Deva9754 <deva.sahu2001@gmail.com>
2025-03-20 17:58:49 +0530

    feat loader added

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,9 +1,34 @@
-import { CircularProgress, Box } from "@mui/material";
+// import { CircularProgress, Box } from "@mui/material";
 
-const Loader = () => (
-  <Box display="flex" justifyContent="center" alignItems="center" height="50vh">
-    <CircularProgress color="primary" size={60} />
-  </Box>
+// const Loader = () => (
+//   <Box display="flex" justifyContent="center" alignItems="center" height="50vh">
+//     <CircularProgress color="primary" size={60} />
+//   </Box>
+// );
+
+// export default Loader;
+
+
+import { CircularProgress, Box, Fade } from "@mui/material";
+
+const Loader = ({ loading = true }) => (
+  <Fade in={loading} timeout={300}>
+    <Box
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      position="fixed"
+      top={0}
+      left={0}
+      width="100%"
+      height="100vh"
+      bgcolor="rgba(0, 0, 0, 0.5)"
+      zIndex={9999}
+      sx={{ backdropFilter: "blur(5px)" }}
+    >
+      <CircularProgress color="primary" size={60} />
+    </Box>
+  </Fade>
 );
 
 export default Loader;
